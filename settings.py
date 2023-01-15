caeabc4086c9eb4c931dfe97dbdc60cca27e25be
andrei0075 <114240378+andrei0075@users.noreply.github.com>
2023-01-15 10:34:12 +0100

    Bullet time

--- settings.py
+++ settings.py
@@ -4,4 +4,9 @@ class Settings:
         self.screen_heigth = 500
         self.bg_color = (0, 0, 0)
         self.ship_speed = 0.15
+        """settings bullet"""
+        self.bullet_speed = 1
+        self.bullet_wdth = 3
+        self.bullet_height = 15
+        self.bullet_color =(60, 60, 60)
 
